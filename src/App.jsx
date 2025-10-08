59e2db01857fed25b9e55fb6d39a9c6df51eee01
Ramgopal39 <areramgopalreddy@gmail.com>
2025-10-09 00:31:15 +0530

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
-import React from "react";  
+import React from "react";
+import {BrowserRouter,  Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return <BrowserRouter>
+    <Header/>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/sign-in" element={<SignIn/>}/>
+            <Route path="/sign-out" element={<SignOut/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+        </Routes>
+    </BrowserRouter>;
 }
